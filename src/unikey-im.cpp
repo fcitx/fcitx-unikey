743031624d00ac1f74c5613834fcb0e10c57ea38
Weng Xuetian <wengxt@gmail.com>
2012-10-22 12:19:06 -0400

    [unikey] ...why there is some typo

--- src/unikey-im.cpp
+++ src/unikey-im.cpp
@@ -313,7 +313,7 @@ INPUT_RETURN_VALUE FcitxUnikeyDoInputPreedit(FcitxUnikey* unikey, FcitxKeySym sy
 
         // auto commit word that never need to change later in preedit string (like consonant - phu am)
         // if macro enabled, then not auto commit. Because macro may change any word
-        if (unikey->ukopt.macroEnabled == 0 && (UnikeyAtWoaardBeginning() || unikey->auto_commit))
+        if (unikey->ukopt.macroEnabled == 0 && (UnikeyAtWordBeginning() || unikey->auto_commit))
         {
             for (i =0; i < sizeof(WordAutoCommit); i++)
             {
